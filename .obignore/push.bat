f9d2376036444a2901279c32f79d0e09504d57a4
Spuddle-Z <Zhang_Yizhou@sjtu.edu.cn>
2024-03-18 13:52:34 +0800

    2024-03-18

---
+++ .obignore/push.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+cd ..
+
+:: 获取当前日期
+for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
+    set day=%%c
+    set month=%%b
+    set year=%%a
+)
+
+:: 将日期格式化为YYYY-MM-DD
+set date=%year%-%month%-%day%
+
+:: 使用日期作为commit信息
+git add *
+git commit -m "%date%"
+git push
--- .obignore/sync.bat
+++ .obignore/sync.bat
@@ -3,20 +3,5 @@ setlocal
 
 cd ..
 
-:: 获取当前日期
-for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
-    set day=%%c
-    set month=%%b
-    set year=%%a
-)
-
-:: 将日期格式化为YYYY-MM-DD
-set date=%year%-%month%-%day%
-
-:: 使用日期作为commit信息
-git add *
-git commit -m "%date%"
-git push
-
 :: 将.obsidian文件夹同步到其它仓库中
 xcopy ".obsidian" "..\Private\.obsidian" /E /I
